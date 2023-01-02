813cf37c5242475e2acb0018bdd01ad4febaa69a
hidekiabe <hideki-abe@hotmail.com>
2023-01-01 21:37:03 -0300

    att

--- src/views/login.js
+++ src/views/login.js
@@ -8,7 +8,8 @@ class Login extends React.Component {
 
     state = {
         email: "",
-        senha: ""
+        senha: "",
+        mensagemErro: null
     }
 
     entrar = () => {
@@ -17,9 +18,10 @@ class Login extends React.Component {
             email:  this.state.email,
             senha:  this.state.senha
         }).then( response => {
-            console.log(response);
+            this.props.history.push('/home');
         }).catch( erro => {
-            console.log(erro.response);
+            console.log('entrou no erro');
+            this.setState({mensagemErro: erro.response.data});
         })
     }
 
@@ -34,6 +36,9 @@ class Login extends React.Component {
                 <div className="col-md-6" style={ {position: 'relative', left: '300px'} }>
                     <div className="bs-docs-section">
                         <Card title="Login">
+                            <div className="row">
+                                <span> {this.state.mensagemErro} </span>
+                            </div>
                             <div className="row">
                                 <div className="col-lg-12">
                                     <div className="bs-component">
